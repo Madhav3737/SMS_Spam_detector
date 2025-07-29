6aee1ac33e2b73e5265214675e55f511c6e4704b
Madhav Appana <madhavappana37@gmail.com>
2025-07-29 13:33:04 +0530

    modified nltk setup

--- nltk_setup.py
+++ nltk_setup.py
@@ -1,21 +1,19 @@
 import nltk
 
 
-import nltk
-
 def ensure_nltk_resources():
     try:
         nltk.data.find('tokenizers/punkt')
     except LookupError:
-        nltk.download('punkt')
+        nltk.download('punkt', download_dir='./nltk_data')
     try:
         nltk.data.find('taggers/averaged_perceptron_tagger')
     except LookupError:
-        nltk.download('averaged_perceptron_tagger')
+        nltk.download('averaged_perceptron_tagger', download_dir='./nltk_data')
     try:
         nltk.data.find('corpora/wordnet')
     except LookupError:
-        nltk.download('wordnet')
+        nltk.download('wordnet', download_dir='./nltk_data')
 
 # Call this during first-time setup
 
